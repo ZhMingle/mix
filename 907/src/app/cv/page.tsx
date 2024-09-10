2519ad930a03e75739bf799db4e9ea66f6c91c30
Mingle <1179912194@qq.com>
2024-09-10 23:09:38 +1200

    tailwind config backgroundImage

--- 907/src/app/cv/page.tsx
+++ 907/src/app/cv/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React, { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import './BalanceBoard.css';
 
 const BalanceBoard = () => {
   const boardRef = useRef(null);
   const requestRef = useRef(null);
+  const [isHovering, setIsHovering] = useState(false);
 
   const handleMouseMove = (event) => {
     const board = boardRef.current;
@@ -16,14 +17,20 @@ const BalanceBoard = () => {
     const rotateX = ((y / rect.height) - 0.5) * 30; // 控制上下倾斜
     const rotateY = ((x / rect.width) - 0.5) * -30; // 控制左右倾斜
 
-    // 使用 requestAnimationFrame 优化动画性能
-    requestRef.current = requestAnimationFrame(() => {
-      board.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    });
+    // 如果正在悬停，清除之前的 requestAnimationFrame 以防止多次触发
+    if (isHovering) {
+      cancelAnimationFrame(requestRef.current);
+
+      // 使用 requestAnimationFrame 优化动画性能
+      requestRef.current = requestAnimationFrame(() => {
+        board.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+      });
+    }
   };
 
   const handleMouseLeave = () => {
     const board = boardRef.current;
+    setIsHovering(false);
 
     // 清除动画帧
     cancelAnimationFrame(requestRef.current);
@@ -32,14 +39,28 @@ const BalanceBoard = () => {
     board.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg)';
   };
 
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  // 确保组件卸载时清除动画帧
+  useEffect(() => {
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []);
+
   return (
+    <div className='bg-main-gradient'>
+
+    
     <div
       ref={boardRef}
       className="balance-board"
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
     >
       Hover over me!
+    </div>
     </div>
   );
 };
